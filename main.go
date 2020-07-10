a38103d4ca4fe77e4fbaa93e30fd92ffcd767e2a
mar-tina <martinakhaemba@gmail.com>
2020-07-10 21:45:20 +0300

    file cleanup

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ var BuildJene JeneInterface
 
 func main() {
 	InstantiateBuilder("trying.go", "trying")
+	BuildJene.DeclarePkg("main")
 	var Oceans = []string{
 		"Arctic",
 		"Southen",
@@ -17,11 +18,35 @@ func main() {
 		"Atlantic",
 		"Pacific",
 	}
-
 	var randos = []int{1, 2, 3, 4, 5, 6, 7, 8}
 
+	// Adding imports
+	BuildJene.Use("fmt")
+
+	// Commiting imports
+	BuildJene.FlushUse()
+
+	// Params. Function input params
+	params := make(map[string]interface{})
+	params["x"] = "int"
+	params["y"] = "string"
+	f := NewFunc("test", "int", params)
+
+	// Declare function variables
+	f.Declare("book", "string", "")
+	f.Declare("otherbook", "string", "Dirty Code")
+	f.Declare("counter", "int", "")
+
+	// Statements to be executed
+	f.StateEq("book", "otherbook")
+	f.State(BuildJene.Log("book", "This book is called "), BuildJene.Log("counter", "Your book is called "), "return 0")
+
+	// End of the function and Commit writes to file
+	f.End()
+	BuildJene.Commit(f)
 	BuildJene.SliceNewWithContent("oceans", "string", Oceans)
 	BuildJene.SliceNewWithContent("randos", "int", randos)
+
 }
 
 func InstantiateBuilder(filename string, pkg string) {
