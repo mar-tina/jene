e4ddeb6d0cd960c1f16409f60b46ed0c7d59d775
mar-tina <martinakhaemba@gmail.com>
2020-07-13 17:45:05 +0300

    feat(implement func and var creation definitions)

---
+++ templates/assets/definition.js
@@ -0,0 +1,47 @@
+export let StackTree = function () {
+  this.objList = [];
+};
+
+StackTree.prototype.addObject = function (obj) {
+  this.objList.push(obj);
+  console.log("All objects", this.objList);
+};
+
+StackTree.prototype.getAll = function () {
+  return this.objList;
+};
+
+export let FuncObject = function (id) {
+  this.name;
+  this.id = id;
+  this.params = [];
+  this.retval;
+};
+
+FuncObject.prototype.addParams = function (param) {
+  this.params.push(param);
+};
+
+FuncObject.prototype.getAll = function () {
+  return this.params;
+};
+
+FuncObject.prototype.getType = function () {
+  return "FuncObject";
+};
+
+FuncObject.prototype.setName = function (name) {
+  this.name = name;
+};
+
+export let DeclarationObject = function () {
+  this.name;
+  this.type;
+  this.value;
+};
+
+DeclarationObject.prototype.definition = function (name, type, value) {
+  this.name = name;
+  this.type = type;
+  this.value = value;
+};
