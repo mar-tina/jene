531a397b0b5621f5267cb3cefe30d151d67c69ed
mar-tina <martinakhaemba@gmail.com>
2020-07-15 21:26:53 +0300

    Added definition for a loop object

--- templates/assets/definition.js
+++ templates/assets/definition.js
@@ -14,21 +14,22 @@ StackTree.prototype.getAll = function () {
 export let FuncObject = function (id) {
   this.name;
   this.id = id;
+  this.declarations = [];
   this.params = [];
   this.retval;
+  this.ktype = "func";
 };
 
 FuncObject.prototype.addParams = function (param) {
   this.params.push(param);
-  console.log("PARAMAS", this.params);
 };
 
 FuncObject.prototype.getAll = function () {
   return this.params;
 };
 
-FuncObject.prototype.getType = function () {
-  return "FuncObject";
+FuncObject.prototype.getTypeOf = function () {
+  return this.ktype;
 };
 
 FuncObject.prototype.setName = function (name) {
@@ -43,14 +44,33 @@ FuncObject.prototype.setReturnValue = function (ret) {
   this.retval = ret;
 };
 
-export let DeclarationObject = function () {
+export let DeclarationObject = function (id) {
+  this.id = id;
   this.name;
   this.type;
   this.value;
+  this.ktype = "stmt";
 };
 
 DeclarationObject.prototype.definition = function (name, type, value) {
   this.name = name;
   this.type = type;
   this.value = value;
+};
+
+DeclarationObject.prototype.getTypeOf = function () {
+  return this.ktype;
+};
+
+export let LoopObject = function (id) {
+  this.id = id;
+  this.name;
+  this.loopValue;
+  this.arg;
+  this.index;
+  this.ktype = "loop";
+};
+
+LoopObject.prototype.getTypeOf = function () {
+  return this.ktype;
 };
