7d5f2f700ae5fbfd05c57992430120e3af259179
mar-tina <martinakhaemba@gmail.com>
2020-07-15 13:21:58 +0300

    feat(create draggable components)

---
+++ templates/assets/components/modules.js
@@ -0,0 +1,48 @@
+import { Shadow, useState } from "../shadow.js";
+import { sanitize } from "../utils.js";
+
+import "./mod.js";
+
+export let Modules = Shadow("modules-el", {
+  onMount: (self) => {},
+
+  getInitialState: (self) => {
+    let state = {
+      name: "",
+    };
+    return useState(state, self);
+  },
+
+  methods: {
+    handleInput: function (e, self) {
+      e.preventDefault();
+      let value = sanitize(e.target.value);
+      self.setState({ name: value }, false);
+    },
+  },
+
+  template: (self) => /*html*/ `
+    <style>
+      .container {
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 60px auto;
+      }
+      .iname {
+        padding: 10px;
+        margin: 20px;
+        border-radius: 4px;
+        border-width: 3px;
+      }
+    </style>
+    <div class="container">
+        <div>
+            <div @bind="name"> ${self.state.name} </div>
+            <input class="iname" @input="handleInput" /> 
+        </div>
+        <div> 
+            <mod-el> </mod-el>  
+        </div>
+    </div>
+  `,
+});
