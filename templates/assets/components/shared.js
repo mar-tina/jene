aa99c788902c247639e38e419f0d5da42a4d64eb
mar-tina <martinakhaemba@gmail.com>
2020-07-16 13:38:08 +0300

    moved method definitions to a shared folder

--- templates/assets/components/shared.js
+++ templates/assets/components/shared.js
@@ -1,10 +1,34 @@
 import {
   createObjectOnCondition,
-  addStmtToFunctionObject,
+  addDeclarationToFunctionObject,
   checkIfAlreadyExists,
   addLoopToFunctionObject,
 } from "../utils.js";
 import { globalProvider } from "../provider.js";
+
+export function handleDropOnPlay(e, self) {
+  e.preventDefault();
+  let existsInFunc = checkIfExistsInFunction(self.state.currentDragged);
+  if (existsInFunc) {
+    self.state.components.push(
+      Array.isArray(existsInFunc) ? existsInFunc[0] : existsInFunc
+    );
+  } else {
+    let data = JSON.parse(e.dataTransfer.getData("module"));
+    self.state.components.push(createObjectOnCondition(data.name));
+  }
+  globalProvider.providers.globalCtx.proxyObject.components =
+    self.state.components;
+}
+
+export function matchTypeToFunctionCall(type) {
+  switch (type) {
+    case "loop":
+      return "loopDragStart";
+    case "stmt":
+      return "stmtDragStart";
+  }
+}
 
 export function handleDragStartLoop(e, self) {
   var loop = {
@@ -12,7 +36,7 @@ export function handleDragStartLoop(e, self) {
     id: e.target.id,
   };
   e.dataTransfer.setData("module", JSON.stringify(loop));
-  console.log("is a drag", e.target.id);
+  self.setState({ currentDragged: e.target.id }, false);
 }
 
 // Unnecessary. will merge with above function
@@ -22,7 +46,7 @@ export function handleDragStartStmt(e, self) {
     id: e.target.id,
   };
   e.dataTransfer.setData("module", JSON.stringify(item));
-  console.log("is a drag", e.target.id);
+  self.setState({ currentDragged: e.target.id }, false);
 }
 
 export function handleDropOnFunc(e, self) {
@@ -31,14 +55,35 @@ export function handleDropOnFunc(e, self) {
   let data = JSON.parse(e.dataTransfer.getData("module"));
   let existingObj = checkIfAlreadyExists(data.id);
   if (existingObj) {
-    addStmtToFunctionObject(e.target.id, self.state.components, existingObj);
+    addDeclarationToFunctionObject(
+      e.target.id,
+      self.state.components,
+      existingObj
+    );
   } else {
     let obj = createObjectOnCondition(data.name);
-    addStmtToFunctionObject(e.target.id, self.state.components, obj);
+    addDeclarationToFunctionObject(e.target.id, self.state.components, obj);
   }
 
   // update ctx provider to trigger page refresh
   globalProvider.providers.globalCtx.proxyObject.components =
     self.state.components;
   console.log(self);
+}
+
+export function checkIfExistsInFunction(id) {
+  let items = globalProvider.providers.globalCtx.proxyObject.components;
+  let component;
+  for (var i in items) {
+    var modules = items[i];
+    if (modules.getTypeOf() == "func") {
+      var decls = modules.declarations;
+      for (var j in decls) {
+        if (decls[j].id == id) {
+          component = modules.declarations.splice(j, 1);
+        }
+      }
+    }
+  }
+  return component;
 }
